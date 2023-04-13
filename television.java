bdea7d174cfecdaeef0a1516b30aaf016abf8f29
izamisa <124897143+izamisa@users.noreply.github.com>
2023-04-13 21:38:23 +0200

    task1

---
+++ television.java
@@ -0,0 +1,48 @@
+public class television {
+}
+{
+    public final int MAX_CHANNEL = 100;
+    public final int MIN_CHANNEL = 1;
+    public final int MAX_VOLUME  = 10;
+    public final int MIN_VOLUME  = 1;
+    public final int DEFAULT_CHANNEL = 3;
+    public final int DEFAULT_VOLUME  = 5;
+
+    private int channel;
+    private int volume;
+
+    // constructors
+    public Television()
+    {
+
+    }
+
+
+    public void increaseChannel()
+    {
+
+    }
+
+
+    public void decreaseChannel()
+    {
+
+    }
+
+    public void selectChannel(int c)
+    {
+
+    }
+
+
+    public void increaseVolume()
+    {
+
+    }
+
+    
+    public void decreaseVolume()
+    {
+
+    }
+}
